45490645c6792827500f08f3a7110fd2c3831d6b
Hayley <hayleyanderson@zoho.com>
2014-10-23 17:39:00 -0400

    Add hashtag capabilities
    * Add ability to find and link to seach for hashtags
    * Add method to view

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -4,4 +4,10 @@ module ShoutsHelper
       link_to match, user_path(match.sub(/@/, ""))
     end.html_safe
   end
+
+  def hashtag_finder(body)
+    body.gsub(/#\w+/) do |match|
+      link_to match, search_path(:query => match)
+    end.html_safe
+  end
 end
